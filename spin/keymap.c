0ba37eb19638eefc87b0698c776d4ebaea8b85ed
zelims <zelims@users.noreply.github.com>
2021-02-04 22:42:57 -0700

    condense workspace keymaps

--- spin/keymap.c
+++ spin/keymap.c
@@ -3,18 +3,12 @@
 #define _BASE       0
 #define _NUMPAD     1
 
-enum custom_macros {
-    WORKSPACELEFT,
-    WORKSPACERIGHT,
-    WORKSPACE_VIEW
-};
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT(
                 KC_F22, KC_F23, KC_F24, TG(_NUMPAD),
                 KC_F20, KC_F21, KC_F22, KC_MEDIA_PLAY_PAUSE,
-                KC_MEDIA_PREV_TRACK, KC_MEDIA_PLAY_PAUSE, KC_MEDIA_NEXT_TRACK, WORKSPACE_VIEW,
-                WORKSPACELEFT, WORKSPACE_VIEW, WORKSPACERIGHT
+                KC_MEDIA_PREV_TRACK, KC_MEDIA_PLAY_PAUSE, KC_MEDIA_NEXT_TRACK, LGUI(KC_TAB),
+                LCTL(LGUI(KC_LEFT)), LGUI(KC_TAB), LCTL(LGUI(KC_RIGHT))
                 ),
     [_NUMPAD] = LAYOUT(/* numpad */
                 KC_KP_0, KC_KP_PLUS, KC_KP_SLASH, KC_TRNS,
@@ -23,44 +17,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                 KC_KP_1, KC_KP_2, KC_KP_3
                 ),
 };
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    switch(keycode) {
-        case WORKSPACELEFT:
-            if (record->event.pressed) {
-                register_code(KC_LCTRL);
-                register_code(KC_LGUI);
-                register_code(KC_LEFT);
-
-                unregister_code(KC_LCTRL);
-                unregister_code(KC_LGUI);
-                unregister_code(KC_LEFT);
-            }
-            return false;
-        case WORKSPACERIGHT:
-            if (record->event.pressed) {
-                register_code(KC_LCTRL);
-                register_code(KC_LGUI);
-                register_code(KC_RIGHT);
-
-                unregister_code(KC_LCTRL);
-                unregister_code(KC_LGUI);
-                unregister_code(KC_RIGHT);
-            }
-            return false;
-        case WORKSPACE_VIEW:
-            if (record->event.pressed) {
-                register_code(KC_LGUI);
-                register_code(KC_TAB);
-
-                unregister_code(KC_LGUI);
-                unregister_code(KC_TAB);
-            }
-            return false;   
-        default:
-            return true;
-    }
-}
 
 void encoder_update_user(uint8_t index, bool clockwise) {
     if (index == 0) { /* First encoder (top) */
