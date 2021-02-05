43b6c276976478c160c0c5eac39f1028c7af5d0d
zelims <zelims@users.noreply.github.com>
2021-02-05 00:18:41 -0700

    add i3 workspace layout

--- spin/keymap.c
+++ spin/keymap.c
@@ -2,19 +2,26 @@
 
 #define _BASE       0
 #define _NUMPAD     1
+#define _WORKSPACE  2
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT(
                 KC_F22, KC_F23, KC_F24, TG(_NUMPAD),
-                KC_F20, KC_F21, KC_F22, KC_MEDIA_PLAY_PAUSE,
+                KC_F20, KC_F21, KC_F22, TG(_WORKSPACE),
                 KC_MEDIA_PREV_TRACK, KC_MEDIA_PLAY_PAUSE, KC_MEDIA_NEXT_TRACK, LGUI(KC_TAB),
                 LCTL(LGUI(KC_LEFT)), LGUI(KC_TAB), LCTL(LGUI(KC_RIGHT))
                 ),
     [_NUMPAD] = LAYOUT(/* numpad */
-                KC_KP_0, KC_KP_PLUS, KC_KP_SLASH, KC_TRNS,
-                KC_KP_7, KC_KP_8, KC_KP_9, KC_TRNS,
-                KC_KP_4, KC_KP_5, KC_KP_6, KC_TRNS,
+                KC_KP_0, KC_KP_PLUS, KC_KP_SLASH, TG(_WORKSPACE),
+                KC_KP_7, KC_KP_8, KC_KP_9, KC_NO,
+                KC_KP_4, KC_KP_5, KC_KP_6, KC_NLCK,
                 KC_KP_1, KC_KP_2, KC_KP_3
+                ),
+    [_WORKSPACE] = LAYOUT(/* numpad */
+                LGUI(KC_0), KC_NO, KC_NO, TG(_BASE),
+                LGUI(KC_7), LGUI(KC_8), LGUI(KC_9), TG(_NUMPAD),
+                LGUI(KC_4), LGUI(KC_5), LGUI(KC_6), KC_NO,
+                LGUI(KC_1), LGUI(KC_2), LGUI(KC_3)
                 ),
 };
 
