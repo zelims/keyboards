c9b710373c5eb35d8ae6083c1bd81b8d4de443dc
zelims <zelims@users.noreply.github.com>
2021-02-08 22:19:54 -0700

    added rgb lighting based on the workspace that is selected

--- spin/keymap.c
+++ spin/keymap.c
@@ -6,15 +6,15 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT(
-                KC_F22, KC_F23, KC_F24, TO(_NUMPAD),
-                KC_F20, KC_F21, KC_F22, TO(_WORKSPACE),
-                KC_MEDIA_PREV_TRACK, KC_MEDIA_PLAY_PAUSE, KC_MEDIA_NEXT_TRACK, LGUI(KC_TAB),
-                LCTL(LGUI(KC_LEFT)), LGUI(KC_TAB), LCTL(LGUI(KC_RIGHT))
+                KC_F22, KC_F23, KC_F24, RGB_TOG,
+                KC_F20, KC_F21, KC_F22, TO(_NUMPAD),
+                KC_MEDIA_PREV_TRACK, KC_MEDIA_PLAY_PAUSE, KC_MEDIA_NEXT_TRACK, TO(_WORKSPACE),
+                KC_NO, KC_NO, KC_NO
                 ),
     [_NUMPAD] = LAYOUT(/* numpad */
-                KC_KP_0, KC_KP_PLUS, KC_KP_SLASH, TO(_WORKSPACE),
-                KC_KP_7, KC_KP_8, KC_KP_9, KC_NO,
-                KC_KP_4, KC_KP_5, KC_KP_6, KC_NLCK,
+                KC_KP_0, KC_KP_PLUS, KC_KP_SLASH, TO(_BASE),
+                KC_KP_7, KC_KP_8, KC_KP_9, KC_NLCK,
+                KC_KP_4, KC_KP_5, KC_KP_6, TO(_WORKSPACE),
                 KC_KP_1, KC_KP_2, KC_KP_3
                 ),
     [_WORKSPACE] = LAYOUT(/* numpad */
@@ -25,36 +25,89 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                 ),
 };
 
+layer_state_t layer_state_set_user(layer_state_t state) {
+    switch (get_highest_layer(state)) {
+        case _BASE:
+            setrgb(RGB_WHITE, &led[0]); 
+            setrgb(0, 0, 0, &led[1]);
+            setrgb(0, 0, 0, &led[2]);
+            break;
+        case _NUMPAD:
+            setrgb(0, 0, 0, &led[0]); 
+            setrgb(RGB_WHITE, &led[1]);
+            setrgb(0, 0, 0, &led[2]);
+            break;
+        case _WORKSPACE:
+            setrgb(0, 0, 0, &led[0]);
+            setrgb(0, 0, 0, &led[1]);
+            setrgb(RGB_WHITE, &led[2]);
+            break;
+    }
+    rgblight_set();
+    return state;
+}
+
+void keyboard_post_init_user(void) {
+    setrgb(RGB_WHITE, &led[0]); 
+    setrgb(0, 0, 0, &led[1]);
+    setrgb(0, 0, 0, &led[2]);
+    rgblight_set();
+}
+
 void encoder_update_user(uint8_t index, bool clockwise) {
     if (index == 0) { /* First encoder (top) */
-        if (clockwise) {
-            tap_code(KC_VOLU); //Example of using tap_code which lets you use keycodes outside of the keymap
-        } else {
-            tap_code(KC_VOLD);
+        switch (get_highest_layer(layer_state)) {
+            case _BASE:
+            case _NUMPAD:
+                if (clockwise) {
+                    tap_code(KC_VOLU);
+                } else {
+                    tap_code(KC_VOLD);
+                }
+                break;
+            case _WORKSPACE:
+                if (clockwise) {
+                    tap_code16(LGUI(KC_DOT));
+                } else {
+                    tap_code16(LGUI(KC_COMMA));
+                }
+                break;
         }
     } else if (index == 1) { /* Second encoder (middle) */  
-        if (clockwise) {
-            tap_code(KC_VOLU); //Example of using tap_code which lets you use keycodes outside of the keymap
-        } else {
-            tap_code(KC_VOLD);
+        switch (get_highest_layer(layer_state)) {
+            case _BASE:
+            case _NUMPAD:
+                if (clockwise) {
+                    tap_code(KC_VOLU);
+                } else {
+                    tap_code(KC_VOLD);
+                }
+                break;
+            case _WORKSPACE:
+                if (clockwise) {
+                    tap_code16(LGUI(KC_DOT));
+                } else {
+                    tap_code16(LGUI(KC_COMMA));
+                }
+                break;
         }
     } else if (index == 2) { /* Third encoder (bottom) */  
-        if (clockwise) {
-            register_code(KC_LCTRL);
-            register_code(KC_LGUI);
-            register_code(KC_RIGHT);
-
-            unregister_code(KC_LCTRL);
-            unregister_code(KC_LGUI);
-            unregister_code(KC_RIGHT);
-        } else {
-            register_code(KC_LCTRL);
-            register_code(KC_LGUI);
-            register_code(KC_LEFT);
-
-            unregister_code(KC_LCTRL);
-            unregister_code(KC_LGUI);
-            unregister_code(KC_LEFT);
+        switch (get_highest_layer(layer_state)) {
+            case _BASE:
+            case _NUMPAD:
+                if (clockwise) {
+                    tap_code(KC_VOLU);
+                } else {
+                    tap_code(KC_VOLD);
+                }
+                break;
+            case _WORKSPACE:
+                if (clockwise) {
+                    tap_code16(LGUI(KC_DOT));
+                } else {
+                    tap_code16(LGUI(KC_COMMA));
+                }
+                break;
         }
     }
 }
